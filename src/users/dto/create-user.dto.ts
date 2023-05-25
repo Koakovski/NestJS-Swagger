54715156fb9bd7d05f3241f846335f0e6cf34f27
Higor Koakovski Pereira <hkoakovskip@gmail.com>
2023-05-25 11:43:37 -0300

    feat: added Swagger decorators to CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    description: "'name' é utilizado para exibir dados no perfil",
+    example: 'Higor Koakovski',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: "'email' será utilizado para autênticação na API",
+    example: 'example@email.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    description: "'password' será utilizada para autênticação na API",
+    example: '@Abc12345',
+  })
+  password: string;
+}
